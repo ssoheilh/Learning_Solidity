63b9e643ef969877b04929eaae1dff77a1b8c164
Soheil <ssoheilhosseini@gmail.com>
2023-01-06 18:53:40 +1100

    Section 4 is done
    :

---
+++ Section_4_Solidity_programming/visibility_solution.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity >=0.5.0 <0.9.0;
+ 
+contract A{
+    int public x = 10;
+
+    function f() internal view returns(int){
+        return x;
+    }
+    
+}
+
+contract B is A{
+    int public xx = f();
+}
